6e58db24661e2b1dc5224eaa154462296a177238
Dswang <wang892947707@hotmail.com>
2024-06-07 23:04:49 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,13 +4,13 @@ addEventListener("fetch", (event) => {
 });
 
 const routes = {
-  "docker.libcuda.so": "https://registry-1.docker.io",
-  "quay.libcuda.so": "https://quay.io",
-  "gcr.libcuda.so": "https://gcr.io",
-  "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-  "k8s.libcuda.so": "https://registry.k8s.io",
-  "ghcr.libcuda.so": "https://ghcr.io",
-  "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
+  "docker.dsdog.tk": "https://registry-1.docker.io",
+  "quay.dsdog.tk": "https://quay.io",
+  "gcr.dsdog.tk": "https://gcr.io",
+  "k8s-gcr.dsdog.tk": "https://k8s.gcr.io",
+  "k8s.dsdog.tk": "https://registry.k8s.io",
+  "ghcr.dsdog.tk": "https://ghcr.io",
+  "cloudsmith.dsdog.tk": "https://docker.cloudsmith.io",
 };
 
 function routeByHosts(host) {
